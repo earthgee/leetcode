14c982ca899119b57ab451b7ea17e81a72b43d09
earthgee <865603556@qq.com>
2024-03-11 23:36:11 +0800

    leetcode

---
+++ src/com/earthgee/interview/Code1143.java
@@ -0,0 +1,25 @@
+package com.earthgee.interview;
+
+public class Code1143 {
+
+    //DP[i][j] represents the longest common subsequence of text1[0 ... i] & text2[0 ... j].
+    public int longestCommonSubsequence(String text1, String text2) {
+        int length1=text1.length();
+        int length2=text2.length();
+        int[][] dp=new int[length1+1][length2+1];
+
+        for(int i=1;i<=length1;i++) {
+            char c1=text1.charAt(i-1);
+            for(int j=1;j<=length2;j++) {
+                char c2=text2.charAt(i-1);
+                if(c1==c2) {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                } else {
+
+                }
+            }
+        }
+
+    }
+
+}
--- src/com/earthgee/interview/Code32.java
+++ src/com/earthgee/interview/Code32.java
@@ -10,13 +10,22 @@ public class Code32 {
             if(s.charAt(i) == ')') {
                 if(s.charAt(i-1) == '(') {
                     dp[i] = (i>=2? dp[i-2]:0) +2;
-                } else if(i-dp[i-1]-1 > 0 && s.charAt(i-dp[i-1]-1) == '(') {
-                    dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2;
+                } else if(i-dp[i-1] > 0 && s.charAt(i-dp[i-1]-1) == '(') {
+                    if(i-dp[i-1]>=2) {
+                        dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2;
+                    } else {
+                        dp[i] = dp[i-1] + 2;
+                    }
                 }
             }
             maxAns = Math.max(maxAns, dp[i]);
         }
         return maxAns;
+    }
+
+    public static void main(String[] args) {
+        Code32 code32=new Code32();
+        System.out.println(code32.longestValidParentheses("(()())"));
     }
 
 }
