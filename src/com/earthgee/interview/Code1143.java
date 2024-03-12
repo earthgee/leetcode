c6c647cb6014a69299dacc178efd4d8f1c021f64
earthgee <865603556@qq.com>
2024-03-12 13:04:27 +0800

    leetcode

--- src/com/earthgee/interview/Code1143.java
+++ src/com/earthgee/interview/Code1143.java
@@ -11,15 +11,17 @@ public class Code1143 {
         for(int i=1;i<=length1;i++) {
             char c1=text1.charAt(i-1);
             for(int j=1;j<=length2;j++) {
-                char c2=text2.charAt(i-1);
+                char c2=text2.charAt(j-1);
+                int max = Math.max(dp[i-1][j], dp[i][j-1]);
                 if(c1==c2) {
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    dp[i][j]=Math.max(dp[i-1][j-1]+1, max);
                 } else {
-
+                    dp[i][j]=max;
                 }
             }
         }
 
+        return dp[length1][length2];
     }
 
 }
